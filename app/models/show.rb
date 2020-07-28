ffec7e7d4595c50cbf1a690716d0882c0bde9182
Travis Gandy <travis_gandy@yahoo.com>
2020-07-28 02:22:39 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
    belongs_to :network
    
    def actors_list
-     self.actors.each{|a| puts a.full_name}
+     self.actors.map{|a| puts a.full_name}
    end 
 end
