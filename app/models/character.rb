f763d6f7cc8cb971c3961832fe68b2e73cae0f52
Travis Gandy <travis_gandy@yahoo.com>
2020-07-28 01:15:40 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
+  attr_accessor :catchphrase
   
+  def catchphrase
+  
+  end 
+  
+  def say_that_thing_you_say
+  "#{self.name} always says: #{self.catchphrase}"
+  end
 end
